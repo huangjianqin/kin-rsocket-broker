08cea982ecbc6445a78d8450646b0d0f6c13cdad
huangjianqin <18814127639@163.com>
2021-04-20 11:27:55 +0800

    代码优化

--- kin-rsocket-service-starter/src/main/java/org/kin/rsocket/springcloud/service/ServicesPublisher.java
+++ kin-rsocket-service-starter/src/main/java/org/kin/rsocket/springcloud/service/ServicesPublisher.java
@@ -15,17 +15,22 @@ import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 
 import java.util.stream.Collectors;
 
 /**
- * 服务暴露给broker
- * todo application ready 时机是否合适
+ * 1. connection构建
+ * 2. send cloud event to broker, 其中包括服务暴露事件
+ * <p>
+ * spring容器refresh完就执行处理, 使用者可以在更低优先级的{@link ApplicationListener<ContextRefreshedEvent>}实例
+ * 或者spring boot事件({@link ApplicationStartedEvent}之后触发的事件)处理自定义逻辑
  *
  * @author huangjianqin
  * @date 2021/3/28
+ * @see ContextRefreshedEvent
  */
-final class ServicesPublisher implements ApplicationListener<ApplicationStartedEvent> {
+final class ServicesPublisher implements ApplicationListener<ContextRefreshedEvent> {
     private static final Logger log = LoggerFactory.getLogger(ServicesPublisher.class);
     @Autowired
     private RSocketServiceConnector connector;
@@ -37,7 +42,7 @@ final class ServicesPublisher implements ApplicationListener<ApplicationStartedE
     private HealthService healthService;
 
     @Override
-    public void onApplicationEvent(ApplicationStartedEvent event) {
+    public void onApplicationEvent(ContextRefreshedEvent event) {
         //connect
         connector.connect(
                 binderBuilderCustomizers.orderedStream().collect(Collectors.toList()),
