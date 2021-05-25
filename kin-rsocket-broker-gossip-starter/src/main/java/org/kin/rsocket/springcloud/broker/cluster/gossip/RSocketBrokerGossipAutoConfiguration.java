0833ecb3b4ce46b91a4433ecdef3d66db09d074c
huangjianqin <18814127639@163.com>
2021-05-26 01:29:13 +0800

    gossip模式判断优化

--- kin-rsocket-broker-gossip-starter/src/main/java/org/kin/rsocket/springcloud/broker/cluster/gossip/RSocketBrokerGossipAutoConfiguration.java
+++ kin-rsocket-broker-gossip-starter/src/main/java/org/kin/rsocket/springcloud/broker/cluster/gossip/RSocketBrokerGossipAutoConfiguration.java
@@ -18,9 +18,12 @@ import org.springframework.core.annotation.Order;
 @Configuration
 @EnableConfigurationProperties(RSocketBrokerGossipProperties.class)
 public class RSocketBrokerGossipAutoConfiguration {
+    /**
+     * 配置了gossip cluster绑定端口, 并且至少有一个seed, 也就是member, 才有效启动gossip 集群模式
+     */
     @Bean
     @ConditionalOnBean(RSocketBrokerProperties.class)
-    @ConditionalOnProperty("kin.rsocket.broker.gossip.port")
+    @ConditionalOnProperty(prefix = "kin.rsocket.broker.gossip", name = {"port", "seeds[0]"})
     public BrokerManager brokerManager(@Autowired RSocketBrokerProperties brokerConfig,
                                        @Autowired RSocketBrokerGossipProperties gossipConfig) {
         return new GossipBrokerManager(brokerConfig, gossipConfig);
