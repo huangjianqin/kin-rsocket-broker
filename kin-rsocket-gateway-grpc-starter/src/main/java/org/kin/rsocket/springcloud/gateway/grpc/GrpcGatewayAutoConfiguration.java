fd47279dbd970e49d13c68f98fb8d0e14d0ae1fb
huangjianqin <18814127639@163.com>
2022-01-19 00:27:24 +0800

    修复其余项目依赖时, 无法注册grpc health service bean异常

--- kin-rsocket-gateway-grpc-starter/src/main/java/org/kin/rsocket/springcloud/gateway/grpc/GrpcGatewayAutoConfiguration.java
+++ kin-rsocket-gateway-grpc-starter/src/main/java/org/kin/rsocket/springcloud/gateway/grpc/GrpcGatewayAutoConfiguration.java
@@ -1,5 +1,8 @@
 package org.kin.rsocket.springcloud.gateway.grpc;
 
+import io.grpc.health.v1.HealthGrpc;
+import org.kin.rsocket.springcloud.gateway.grpc.health.HealthServiceImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -8,4 +11,8 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class GrpcGatewayAutoConfiguration {
+    @Bean
+    public HealthGrpc.HealthImplBase grpcHealthService() {
+        return new HealthServiceImpl();
+    }
 }
