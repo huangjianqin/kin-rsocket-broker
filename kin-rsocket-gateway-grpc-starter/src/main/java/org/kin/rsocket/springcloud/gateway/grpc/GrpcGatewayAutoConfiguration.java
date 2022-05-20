0f1c8ddddd1d142d5db0ae004c4987d9b6a160a7
huangjianqin <18814127639@163.com>
2022-05-20 23:12:58 +0800

    支持自定义HealthGrpc.HealthImplBase

--- kin-rsocket-gateway-grpc-starter/src/main/java/org/kin/rsocket/springcloud/gateway/grpc/GrpcGatewayAutoConfiguration.java
+++ kin-rsocket-gateway-grpc-starter/src/main/java/org/kin/rsocket/springcloud/gateway/grpc/GrpcGatewayAutoConfiguration.java
@@ -2,6 +2,7 @@ package org.kin.rsocket.springcloud.gateway.grpc;
 
 import io.grpc.health.v1.HealthGrpc;
 import org.kin.rsocket.springcloud.gateway.grpc.health.HealthServiceImpl;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GrpcGatewayAutoConfiguration {
     @Bean
+    @ConditionalOnMissingBean(HealthGrpc.HealthImplBase.class)
     public HealthGrpc.HealthImplBase grpcHealthService() {
         return new HealthServiceImpl();
     }
