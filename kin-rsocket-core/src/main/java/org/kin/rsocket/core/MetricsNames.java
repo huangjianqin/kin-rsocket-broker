1e513c7d41ca7aad020d950f37acc5a67cb1fbd0
huangjianqin <459564034@qq.com>
2022-11-22 23:33:28 +0800

    监控指标命名修改

--- kin-rsocket-core/src/main/java/org/kin/rsocket/core/MetricsNames.java
+++ kin-rsocket-core/src/main/java/org/kin/rsocket/core/MetricsNames.java
@@ -10,13 +10,13 @@ public interface MetricsNames {
 
     //-----------------------------------------------broker-----------------------------------------------
     /** 集群broker数量 */
-    String CLUSTER_BROKER_COUNT = "cluster.broker".concat(COUNT_SUFFIX);
+    String CLUSTER_BROKER_COUNT = "rsocket.cluster.broker".concat(COUNT_SUFFIX);
     /** broker连接的app数量 */
-    String BROKER_APPS_COUNT = "broker.apps".concat(COUNT_SUFFIX);
+    String BROKER_APPS_COUNT = "rsocket.broker.apps".concat(COUNT_SUFFIX);
     /** broker连接的对外提供服务app实例数量 */
-    String BROKER_SERVICE_PROVIDER_COUNT = "broker.service.provider".concat(COUNT_SUFFIX);
+    String BROKER_SERVICE_PROVIDER_COUNT = "rsocket.broker.service.provider".concat(COUNT_SUFFIX);
     /** broker暴露的服务数量 */
-    String BROKER_SERVICE_COUNT = "broker.service".concat(COUNT_SUFFIX);
+    String BROKER_SERVICE_COUNT = "rsocket.broker.service".concat(COUNT_SUFFIX);
     /** broker接受upstream服务请求次数 */
     String RSOCKET_REQUEST_COUNT = "rsocket.request".concat(COUNT_SUFFIX);
 
