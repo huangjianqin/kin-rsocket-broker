f24a5234dce2cc58afce99d63e1e63272e91108b
huangjianqin <459564034@qq.com>
2023-03-11 23:26:57 +0800

    cbor对象序列化优化

--- kin-rsocket-core/src/main/java/org/kin/rsocket/core/codec/CborObjectCodec.java
+++ kin-rsocket-core/src/main/java/org/kin/rsocket/core/codec/CborObjectCodec.java
@@ -1,6 +1,11 @@
 package org.kin.rsocket.core.codec;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.dataformat.cbor.CBORFactory;
 import io.netty.buffer.*;
 import io.netty.util.ReferenceCountUtil;
@@ -16,7 +21,22 @@ import java.io.OutputStream;
  * @date 2021/4/20
  */
 public class CborObjectCodec implements ObjectCodec {
-    private static final ObjectMapper MAPPER = new ObjectMapper(new CBORFactory());
+    private static final ObjectMapper PARSER = new ObjectMapper(new CBORFactory());
+
+    static {
+        PARSER.findAndRegisterModules();
+        //允许json中含有指定对象未包含的字段
+        PARSER.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        //允许序列化空对象
+        PARSER.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+        //不序列化默认值, 0,false,[],{}等等, 减少json长度
+        PARSER.setDefaultPropertyInclusion(JsonInclude.Include.NON_DEFAULT);
+        //只认field, 那些get set is开头的方法不生成字段
+        PARSER.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+        PARSER.setVisibility(PropertyAccessor.SETTER, JsonAutoDetect.Visibility.NONE);
+        PARSER.setVisibility(PropertyAccessor.GETTER, JsonAutoDetect.Visibility.NONE);
+        PARSER.setVisibility(PropertyAccessor.IS_GETTER, JsonAutoDetect.Visibility.NONE);
+    }
 
     @Override
     public RSocketMimeType mimeType() {
@@ -29,7 +49,7 @@ public class CborObjectCodec implements ObjectCodec {
             ByteBuf byteBuf = PooledByteBufAllocator.DEFAULT.buffer(DEFAULT_BUFFER_SIZE);
             try {
                 ByteBufOutputStream bos = new ByteBufOutputStream(byteBuf);
-                MAPPER.writeValue((OutputStream) bos, args);
+                PARSER.writeValue((OutputStream) bos, args);
                 return byteBuf;
             } catch (Exception e) {
                 ReferenceCountUtil.safeRelease(byteBuf);
@@ -44,7 +64,7 @@ public class CborObjectCodec implements ObjectCodec {
     public Object decodeParams(ByteBuf data, Class<?>... targetClasses) throws ObjectCodecException {
         if (data.readableBytes() > 0 && targetClasses != null && targetClasses.length > 0) {
             try {
-                return MAPPER.readValue((InputStream) new ByteBufInputStream(data), Object[].class);
+                return PARSER.readValue((InputStream) new ByteBufInputStream(data), Object[].class);
             } catch (Exception e) {
                 ExceptionUtils.throwExt(e);
             }
@@ -59,7 +79,7 @@ public class CborObjectCodec implements ObjectCodec {
             ByteBuf byteBuf = PooledByteBufAllocator.DEFAULT.buffer(DEFAULT_BUFFER_SIZE);
             try {
                 ByteBufOutputStream bos = new ByteBufOutputStream(byteBuf);
-                MAPPER.writeValue((OutputStream) bos, result);
+                PARSER.writeValue((OutputStream) bos, result);
                 return byteBuf;
             } catch (Exception e) {
                 ReferenceCountUtil.safeRelease(byteBuf);
@@ -73,7 +93,7 @@ public class CborObjectCodec implements ObjectCodec {
     public Object decodeResult(ByteBuf data, Class<?> targetClass) throws ObjectCodecException {
         if (data.readableBytes() > 0 && targetClass != null) {
             try {
-                return MAPPER.readValue((InputStream) new ByteBufInputStream(data), targetClass);
+                return PARSER.readValue((InputStream) new ByteBufInputStream(data), targetClass);
             } catch (Exception e) {
                 ExceptionUtils.throwExt(e);
             }
