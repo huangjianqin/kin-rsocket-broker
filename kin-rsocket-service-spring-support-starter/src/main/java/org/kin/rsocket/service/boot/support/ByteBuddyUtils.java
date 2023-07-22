aaa2a3ec28fc857cff8a063b20a7cff5c5dc9f00
huangjianqin <459564034@qq.com>
2023-07-22 21:27:49 +0800

    修复同服务下创建多次requester仍然得到同一实例异常

--- kin-rsocket-service-spring-support-starter/src/main/java/org/kin/rsocket/service/boot/support/ByteBuddyUtils.java
+++ kin-rsocket-service-spring-support-starter/src/main/java/org/kin/rsocket/service/boot/support/ByteBuddyUtils.java
@@ -6,37 +6,27 @@ import net.bytebuddy.implementation.MethodDelegation;
 import net.bytebuddy.matcher.ElementMatchers;
 import org.kin.framework.utils.ExceptionUtils;
 
-import java.util.Map;
-import java.util.Objects;
-import java.util.concurrent.ConcurrentHashMap;
-
 /**
  * @author huangjianqin
  * @date 2021/3/27
  */
 final class ByteBuddyUtils {
-    /** key -> rsocket service interface, value -> 对应的proxy class */
-    private static final Map<Class<?>, Class<?>> SERVICE_INTERFACE_CLASS_2_PROXY_CLASS = new ConcurrentHashMap<>();
 
     private ByteBuddyUtils() {
     }
 
     @SuppressWarnings("unchecked")
     public static <T> T build(Class<T> serviceInterface, Object proxy) {
-        Class<T> proxyClass = (Class<T>) SERVICE_INTERFACE_CLASS_2_PROXY_CLASS.get(serviceInterface);
-        if (Objects.isNull(proxyClass)) {
-            proxyClass = (Class<T>) new ByteBuddy(ClassFileVersion.JAVA_V8)
-                    .subclass(serviceInterface)
-                    .name(serviceInterface.getSimpleName() + "RSocketStub")
-                    //过滤默认方法和Object定义的方法
-                    .method(ElementMatchers.not(ElementMatchers.isDefaultMethod())
-                            .and(ElementMatchers.not(ElementMatchers.isDeclaredBy(Object.class))))
-                    .intercept(MethodDelegation.to(proxy))
-                    .make()
-                    .load(serviceInterface.getClassLoader())
-                    .getLoaded();
-            SERVICE_INTERFACE_CLASS_2_PROXY_CLASS.put(serviceInterface, proxyClass);
-        }
+        Class<T> proxyClass = (Class<T>) new ByteBuddy(ClassFileVersion.JAVA_V8)
+                .subclass(serviceInterface)
+                .name(serviceInterface.getSimpleName() + "RSocketStub")
+                //过滤默认方法和Object定义的方法
+                .method(ElementMatchers.not(ElementMatchers.isDefaultMethod())
+                        .and(ElementMatchers.not(ElementMatchers.isDeclaredBy(Object.class))))
+                .intercept(MethodDelegation.to(proxy))
+                .make()
+                .load(serviceInterface.getClassLoader())
+                .getLoaded();
 
         try {
             return proxyClass.newInstance();
